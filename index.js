0d8e8e78bf3f7b1bdf60706e00d2e62d68f6d4ff
Ryan Guthrie <ryanguthrie@zoho.com>
2021-08-08 22:37:43 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+document.querySelector('main').remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = "victory"
+
+document.getElementById("victory")
+newHeader.innerHTML = "<h1>Ryan is the champion</h1>"
