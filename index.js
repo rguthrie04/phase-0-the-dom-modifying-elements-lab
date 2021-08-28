fbce08d9f2d7b2f3765209a1caabd468892d271c
Ryan Guthrie <ryanguthrie@zoho.com>
2021-08-28 20:53:44 +0100

    commit tree 962cb9082d381abe503a5e2babb2fa2cd5377b16

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 document.querySelector('main').remove()
 
-const newHeader = document.createElement('h1')
-newHeader.id = "victory"
-
-document.getElementById("victory")
-newHeader.innerHTML = "<h1>Ryan is the champion</h1>"
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.textContent = 'Ryan is the champion'
